b930d12841e5476ccb4f342536229bded2ffd053
Muhammed Furkan Baltacı <170101065@ogrenci.yalova.edu.tr>
2021-03-14 00:05:08 +0300

    13 Mart End Commit

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -33,4 +33,16 @@ export class CategoryComponent implements OnInit {
       return 'list-group-item';
     }
   }
+
+  getAllCategoryClass() {
+    if (!this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
+  }
+
+  clearCurrentCategory() {
+    //this.currentCategory =
+  }
 }
